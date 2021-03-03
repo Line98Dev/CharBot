dc2e88203f5a101152c2cea1a0f59a94b6166c5a
Hunter Line <hunter@line98.dev>
2021-03-03 16:10:51 -0500

    Reworked script

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
-echo Building CharBot
+echo Pulling any changes
+git pull
+echo Stopping CharBot service
 sudo systemctl stop CharBot
-cd CharBot
+echo Building CharBot
 dotnet publish -c release -r ubuntu.20.10-arm64 --self-contained
+echo CharBot built
 sudo systemctl start CharBot
-echo CharBot built, service started
+echo service started
