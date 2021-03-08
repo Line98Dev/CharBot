bff6cc0f3a5cb8559b842e8525cd507ce5aca524
Hunter <hunter@line98.dev>
2021-03-08 13:40:20 -0500

    Removed sudo from build script

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 echo Pulling any changes
 git pull
 echo Stopping CharBot service
-sudo systemctl --user stop charbot
+systemctl --user stop charbot
 echo Building CharBot
 dotnet publish -c release -r ubuntu.20.10-arm64 --self-contained
 echo CharBot built
-sudo systemctl --user start charbot
+systemctl --user start charbot
 echo service started
