c13d9ac03a82d2e86576d5fb6ce002cf4e6c1cec
Hunter <hunter@line98.dev>
2021-03-08 13:38:05 -0500

    updated build script for user profile

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 echo Pulling any changes
 git pull
 echo Stopping CharBot service
-sudo systemctl stop CharBot
+sudo systemctl --user stop charbot
 echo Building CharBot
 dotnet publish -c release -r ubuntu.20.10-arm64 --self-contained
 echo CharBot built
-sudo systemctl start CharBot
+sudo systemctl --user start charbot
 echo service started
